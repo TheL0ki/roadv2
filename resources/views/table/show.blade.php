6bc214caef8338583cbb9becf615a0c9b4699a25
TheL0ki <theloki1712@gmail.com>
2024-04-08 11:36:56 +0200

    display schedule on the correct dates

--- resources/views/table/show.blade.php
+++ resources/views/table/show.blade.php
@@ -1,26 +1,30 @@
 <x-layout>
     <x-slot:heading>Home</x-slot:heading>
-    <?php // dd($user); ?>
+    <?php // dd($table); ?>
     <div class="container-fluid">
-        <table class="table table-bordered table-striped">
-            <thead>
-                <th>Name</th>
-                @for ($i = 1; $i <= 31; $i++)
-                    <th>{{ $i }}</th>
-                @endfor
-            </thead>
-            <tbody>
-                @foreach ($table as $item)
-                    <tr>
-                        <td>{{ $item->firstName }} {{ $item->lastName }}</td>
-                        @for ($i = 1; $i <= 31; $i++)
-                            <td>
-                                {{ $item->shift }}
-                            </td>
-                        @endfor
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+        <div class="table-responsive">
+            <table class="table table-bordered table-striped">
+                <thead>
+                    <th>Name</th>
+                    @for ($i = 1; $i <= $date->format('t'); $i++)
+                        <th>{{ $i }}</th>
+                    @endfor
+                </thead>
+                <tbody>
+                    @foreach ($user as $item)
+                        <tr>
+                            <td><a href="/schedule/change/{{ $item->id }}"> {{ $item->firstName }} {{ $item->lastName }} </a></td>
+                            @for ($i = 1; $i <= $date->format('t'); $i++)
+                                <td>
+                                    @isset ($table[$item->id][$i])
+                                        {{ $table[$item->id][$i]->shift->display }}
+                                    @endisset
+                                </td>
+                            @endfor
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,52 @@ use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    
+    $date = new DateTime();
 
-    $user = User::with(['schedule' => function ($query) {
-        $query->with('shift')->where('month', "=", 4);
+    $user = User::with(['schedule' => function ($query) use ($date) {
+        $query->with('shift')->where('month', '=', $date->format('n'))->where('year', '=', $date->format('Y'));
     }])->get();
 
+    $table = [];
+
+    foreach ($user as $item) {
+        foreach ($item->schedule as $entry) {
+            $table[$entry->user_id][$entry->day] = $entry;
+        }
+    }
+
+    $date = new DateTime();
+
     return view('table.show', [
-        'table' => $user,
-        'year' => 2024,
-        'month' => 4
+        'user' => $user,
+        'table' => $table,
+        'date' => $date
     ]);
+});
+
+Route::get('/schedule/{year}/{month}', function($year, $month) {
+    
+    $displayDate = $year . '-' . $month . '-01';
+    $date = new DateTime($displayDate);
+
+    $user = User::with(['schedule' => function ($query) use ($date) {
+        $query->with('shift')->where('month', '=', $date->format('n'))->where('year', '=', $date->format('Y'));
+    }])->get();
+
+    $table = [];
+
+    foreach ($user as $item) {
+        foreach ($item->schedule as $entry) {
+            $table[$entry->user_id][$entry->day] = $entry;
+        }
+    }
+
+    return view('table.show', [
+        'user' => $user,
+        'table' => $table,
+        'date' => $date
+    ]); 
 });
 
 Route::get('/settings', function () {
