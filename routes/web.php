6027865e06f77deef7766d2262b313b8a7c27992
TheL0ki <theloki1712@gmail.com>
2024-05-24 16:23:06 +0200

    Login Form & Middleware

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,18 @@
 <?php
 
+use App\Http\Middleware\Role;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TeamController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\BatchController;
 use App\Http\Controllers\ShiftController;
+use App\Http\Controllers\SessionController;
 use App\Http\Controllers\ScheduleController;
 use App\Http\Controllers\SettingsController;
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
+Route::delete('/logout', [SessionController::class, 'destroy']);
 
 Route::get('/', [ScheduleController::class, 'index']);
 Route::get('/schedule/change/{id}', [ScheduleController::class, 'edit']);
@@ -14,10 +20,10 @@ Route::get('/schedule/{year}/{month}', [ScheduleController::class, 'show']);
 
 Route::get('/settings', [SettingsController::class, 'index']);
 
-Route::get('/batch',[BatchController::class, 'index']);
+Route::get('/batch', [BatchController::class, 'index'])->middleware(Role::class);
 
-Route::get('/userManagement', [UserController::class, 'index']);
+Route::get('/userManagement', [UserController::class, 'index'])->middleware(Role::class);
 
-Route::get('/teamManagement',[TeamController::class, 'index']);
+Route::get('/teamManagement', [TeamController::class, 'index'])->middleware(Role::class);
 
-Route::get('/shiftManagement', [ShiftController::class, 'index']);
+Route::get('/shiftManagement', [ShiftController::class, 'index'])->middleware(Role::class);
