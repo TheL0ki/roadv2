6d7a39118a47277ef7077fba782133dcddffce6d
TheL0ki <theloki1712@gmail.com>
2025-03-04 13:16:57 +0100

    bugfix js assets

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/js/dropdownMenu.js',
+                'resources/js/mobileMenu.js',
             ],
             refresh: true,
         }),
